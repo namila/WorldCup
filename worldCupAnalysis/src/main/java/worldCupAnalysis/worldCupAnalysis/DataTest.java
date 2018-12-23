6e3681f669c2372b03d006e2ad1707643f244ee6
Namila Perera <NamilaP@99x.lk>
2018-12-23 18:24:06 +0530

    Test task added

--- worldCupAnalysis/src/main/java/worldCupAnalysis/worldCupAnalysis/DataTest.java
+++ worldCupAnalysis/src/main/java/worldCupAnalysis/worldCupAnalysis/DataTest.java
@@ -1,5 +1,32 @@
 package worldCupAnalysis.worldCupAnalysis;
 
-public class DataTest {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
+import utils.LogTypes;
+import utils.Utilities;
+
+public class DataTest {
+	public static void main(String[] args) throws IOException {
+		int lineCount = 0;
+		File sampleDataFile = new File("TestData/test1.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(sampleDataFile));
+
+		String fileLine = reader.readLine();
+
+		while (fileLine != null) {
+			if (Utilities.isRelatedLogLine(fileLine, LogTypes.PAGE_VIEWS)) {
+				lineCount += 1;
+				System.out.println(fileLine);
+			}
+			fileLine = reader.readLine();
+		}
+		
+		System.out.println("Total Count "+ lineCount);
+	}
 }
