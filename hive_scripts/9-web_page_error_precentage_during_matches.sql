dbb4b104d441cee3ab89dee720efbbebeb357d5b
Namila Perera <NamilaP@99x.lk>
2018-12-30 19:06:52 +0530

    Hive script added to load time table data

--- hive_scripts/9-web_page_error_precentage_during_matches.sql
+++ hive_scripts/9-web_page_error_precentage_during_matches.sql
@@ -1,2 +1,5 @@
 USE worldcup;
 
+-- Trying to do a map side join if possbile
+SET hive.auto.convert.join = true;
+
---
+++ hive_scripts/load_time_table.sql
@@ -0,0 +1,44 @@
+USE worldcup;
+
+-- Raw table for time table data
+CREATE TABLE IF NOT EXISTS timeTableRaw(
+  matchDate STRING,
+  startTime STRING,
+  endTime STRING,
+  team1 STRING,
+  team2 STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
+
+-- Loading time table data into raw table
+LOAD DATA LOCAL INPATH  '/resource/Data/Schedule' OVERWRITE INTO TABLE timeTableRaw;
+
+-- Making it possbile to have all partitions as dynamic
+SET hive.exec.dynamic.partition.mode=nonstrict;
+SET hive.decode.partition.name=true;
+
+-- Creating the cleaned data table
+CREATE TABLE IF NOT EXISTS timeTable(
+  startTime STRING,
+  endTime STRING,
+  team1 STRING,
+  team2 STRING
+)
+PARTITIONED BY (matchDate STRING)
+STORED AS TEXTFILE;
+
+-- Loading time table formatted data
+FROM timeTableRaw Source
+
+INSERT OVERWRITE TABLE timeTable PARTITION(matchDate)
+SELECT Source.startTime, Source.endTime, Source.team1, Source.team2,
+  REGEXP_REPLACE(Source.matchDate, "/", "-");
+
+
+
+
+
+
+
