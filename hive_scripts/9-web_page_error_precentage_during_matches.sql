588c8f9e6db6e6b732bc19fc8106d6ec82cca6aa
Namila Perera <NamilaP@99x.lk>
2018-12-31 15:44:56 +0530

    Web page errors percentage added

--- hive_scripts/9-web_page_error_precentage_during_matches.sql
+++ hive_scripts/9-web_page_error_precentage_during_matches.sql
@@ -1,5 +1,9 @@
 USE worldcup;
+SELECT SUM(IF(logData.statusCode != 200,1,0))/COUNT(*) * 100 AS ErrorPercentage
+FROM logData JOIN timeTable ON (logData.logDate == timeTable.matchDate) 
+WHERE logData.requestType = "GET" AND logData.isHTML = true AND unix_timestamp(substr(logData.logTimeStamp, 13),"HH:mm:ss") >= unix_timestamp(timeTable.startTime,"HH:mm") AND unix_timestamp(substr(logData.logTimeStamp, 13),"HH:mm:ss") <= unix_timestamp(timeTable.endTime,"HH:mm");
 
--- Trying to do a map side join if possbile
-SET hive.auto.convert.join = true;
+
+
+
 
